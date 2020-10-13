a9c6d92347ae1cc002cc2b58ef63f3a7d22a7481
kazalghosh <mrgkazal@gmail.com>
2020-10-13 16:28:40 +0600

    final update

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const ObjectId = require('mongodb').ObjectID
 
 require('dotenv').config()
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uhlcz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
 
 const app = express()
 
@@ -70,8 +68,5 @@ client.connect(err => {
     })
 
 });
-
-
-
 
 app.listen(process.env.PORT || port)
